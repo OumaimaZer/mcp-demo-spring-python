15c529cd3c66092d3fe85a76ff9b8b58a54e8fee
OumaimaZer <o.zerhouani@edu.umi.ac.ma>
2025-07-07 00:11:01 +0100

    1

--- mcp-client/src/main/java/net/zerhouani/mcpclient/McpClientApplication.java
+++ mcp-client/src/main/java/net/zerhouani/mcpclient/McpClientApplication.java
@@ -1,13 +1,34 @@
 package net.zerhouani.mcpclient;
 
+import io.modelcontextprotocol.client.McpSyncClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class McpClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(McpClientApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(List<McpSyncClient> clients) {
+        return args -> {
+            clients.forEach(client -> {
+                client.listTools().tools().forEach(tool->{
+                    System.out.println("-------------------");
+                    System.out.println(tool.name());
+                    System.out.println(tool.description());
+                    System.out.println(tool.inputSchema());
+                    System.out.println("-------------------");
+                });
+            });
+        };
+
     }
 
 }
