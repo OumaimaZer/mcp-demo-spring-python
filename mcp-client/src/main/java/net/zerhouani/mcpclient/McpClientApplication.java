52d033bc9fefe39e5fde139cd00953f6778d4626
OumaimaZer <o.zerhouani@edu.umi.ac.ma>
2025-07-07 16:38:49 +0100

    chat on swagger

--- mcp-client/src/main/java/net/zerhouani/mcpclient/McpClientApplication.java
+++ mcp-client/src/main/java/net/zerhouani/mcpclient/McpClientApplication.java
@@ -1,6 +1,7 @@
 package net.zerhouani.mcpclient;
 
 import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.spec.McpSchema;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,21 +15,25 @@ public class McpClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(McpClientApplication.class, args);
     }
-
     @Bean
     CommandLineRunner run(List<McpSyncClient> clients) {
         return args -> {
             clients.forEach(client -> {
-                client.listTools().tools().forEach(tool->{
-                    System.out.println("-------------------");
+                client.listTools().tools().forEach(tool -> {
+                    System.out.println("***************");
                     System.out.println(tool.name());
-                    System.out.println(tool.description());
                     System.out.println(tool.inputSchema());
-                    System.out.println("-------------------");
+                    System.out.println(tool.description());
+                    System.out.println("************");
                 });
             });
+            var params= """
+                    {
+                     "companyName":"OCP"
+                    }
+                    """;
+            McpSchema.CallToolResult result = clients.get(0).callTool(new McpSchema.CallToolRequest("getStockByCompany", params));
+            System.out.println(result.content().get(0).type());
         };
-
     }
-
 }
